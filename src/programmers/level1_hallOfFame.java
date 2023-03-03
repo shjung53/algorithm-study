b1d2054c1f98947f98436c66aff80a7c7ef175e7
shjung <gcjung777@gmail.com>
2023-03-03 17:49:06 +0900

    23.03.03

---
+++ src/programmers/level1_hallOfFame.java
@@ -0,0 +1,31 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class level1_hallOfFame {
+    class Solution {
+        public int[] solution(int k, int[] score) {
+            int[] answer = new int[score.length];
+            ArrayList<Integer> hof = new ArrayList<>();
+
+            for(int i=0; i<score.length;i++){
+                if(hof.size()<k){
+                    hof.add(score[i]);
+                    Collections.sort(hof);
+                    answer[i]=hof.get(0);
+                    continue;
+                }
+
+                hof.add(score[i]);
+                Collections.sort(hof);
+                hof.remove(0);
+                answer[i] = hof.get(0);
+            }
+
+
+
+            return answer;
+        }
+    }
+}
