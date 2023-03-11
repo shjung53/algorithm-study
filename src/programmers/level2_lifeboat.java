5ce5c8db63ec3f76da289ddc05b1e5632f248cbd
shjung <gcjung777@gmail.com>
2023-03-11 18:54:24 +0900

    23.03.11

---
+++ src/programmers/level2_lifeboat.java
@@ -0,0 +1,17 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class level2_lifeboat {
+    class Solution {
+        public int solution(int[] people, int limit) {
+            Arrays.sort(people);
+            int i = 0, j = people.length - 1;
+            for (; i < j; --j) {
+                if (people[i] + people[j] <= limit)
+                    ++i;
+            }
+            return people.length - i;
+        }
+    }
+}
