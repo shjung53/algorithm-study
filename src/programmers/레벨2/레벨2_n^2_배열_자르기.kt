ad83e94759a6a3eaab162c51709cf5ff83c70196
shjung <gcjung777@gmail.com>
2023-03-17 15:22:41 +0900

    23.03.17

---
+++ src/programmers/레벨2/레벨2_n^2_배열_자르기.kt
@@ -0,0 +1,12 @@
+package programmers.레벨2
+
+class `레벨2_n^2_배열_자르기` {
+    fun 내풀이(n: Int, left: Long, right: Long): IntArray {
+        var answer: IntArray = IntArray(n*n)
+
+
+
+
+        return answer
+    }
+}
