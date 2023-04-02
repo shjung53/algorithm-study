ae76892d7903bd54f593fe6a738e9e198f47dfa2
shjung53 <gcjung777@gmail.com>
2023-04-03 00:34:51 +0900

    23.04.02

--- src/programmers/레벨2/할인_행사.kt
+++ src/programmers/레벨2/할인_행사.kt
@@ -1,30 +1,38 @@
 package programmers.레벨2
 
-import com.sun.xml.internal.fastinfoset.util.StringArray
-
 class 할인_행사 {
     fun solution(want: Array<String>, number: IntArray, discount: Array<String>): Int {
         var answer: Int = 0
 
-        val wants = LinkedHashSet<String>(10)
-        val items = LinkedHashSet<String>(10)
+        val wants = HashMap<String,Int>()
+        val items = HashMap<String,Int>()
 
         for(i in want.indices){
             for(j in 1 .. number[i]){
-                wants.add(want[i])
+                wants[want[i]] = wants.getOrDefault(want[i],0)+1
             }
         }
 
+        out@
         for(i in 0 .. discount.size-10){
             for(j in i .. i+9){
-                items.add(discount[j])
+                items[discount[j]] = items.getOrDefault(discount[j],0)+1
             }
-            if(wants.toSet()==items.toSet()){
-                answer++
+            for(item in items.keys){
+                if(wants[item]==null || wants[item] != items[item]){
+                    items.clear()
+                    continue@out
+                }
             }
+            answer++
             items.clear()
         }
 
         return answer
     }
+}
+
+fun main() {
+    val a = 할인_행사()
+    a.solution(arrayOf("banana", "apple", "rice", "pork", "pot"),	intArrayOf(3, 2, 2, 2, 1),	arrayOf("chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"))
 }
