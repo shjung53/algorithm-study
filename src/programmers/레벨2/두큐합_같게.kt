078127c0bcaa858d31e8628187e4a7c82b56c3ab
unknown <shjung53@github.com>
2023-04-28 21:54:31 +0900

    23.04.28

--- src/programmers/레벨2/두큐합_같게.kt
+++ src/programmers/레벨2/두큐합_같게.kt
@@ -3,7 +3,7 @@ package programmers.레벨2
 import java.util.*
 import kotlin.collections.ArrayDeque
 
-class Solution {
+class 두큐합_같게 {
     fun solution(queue1: IntArray, queue2: IntArray): Int {
         var answer: Int = 0
 
@@ -11,6 +11,7 @@ class Solution {
         val q2 = LinkedList<Int>()
         var sum1 = 0L
         var sum2 = 0L
+        val total = queue1 + queue2
 
         val case = LinkedList<Bfs>()
 
@@ -24,10 +25,12 @@ class Solution {
             sum2 += i
         }
 
-        if(sum1 == sum2) return 0
+        Arrays.sort(total)
 
-        case.offer(Bfs(q1, q2, sum1, sum2, 0, 1))
-        case.offer(Bfs(q1, q2, sum1, sum2, 0, 2))
+        if(!checkPossible(total, sum1 + sum2)) return -1
+
+        case.offer(Bfs(LinkedList<Int>(q1), LinkedList<Int>(q2), sum1, sum2, 0, 1))
+        case.offer(Bfs(LinkedList<Int>(q1), LinkedList<Int>(q2), sum1, sum2, 0, 2))
 
         while(case.isNotEmpty()) {
             val bfs = case.poll()
@@ -36,25 +39,45 @@ class Solution {
                 break
             }
 
+            if(bfs.q1.isEmpty() || bfs.q2.isEmpty()) continue
+
             if(bfs.turn == 1) {
                 val poll = bfs.q1.poll()
-                bfs.sum1 -= poll
                 bfs.q2.offer(poll)
-                bfs.sum2 += poll
-                case.offer(Bfs(bfs.q1, bfs.q2, bfs.sum1, bfs.sum2, bfs.count++, 1))
-                case.offer(Bfs(bfs.q1, bfs.q2, bfs.sum1, bfs.sum2, bfs.count++, 2))
+                case.offer(Bfs(LinkedList<Int>(bfs.q1), LinkedList<Int>(bfs.q2), bfs.sum1 - poll, bfs.sum2 + poll, bfs.count+1, 1))
+                case.offer(Bfs(LinkedList<Int>(bfs.q1), LinkedList<Int>(bfs.q2), bfs.sum1 - poll, bfs.sum2 + poll, bfs.count+1, 2))
             } else {
                 val poll = bfs.q2.poll()
-                bfs.sum2 -= poll
                 bfs.q1.offer(poll)
-                bfs.sum1 += poll
-                case.offer(Bfs(bfs.q1, bfs.q2, bfs.sum1, bfs.sum2, bfs.count++, 1))
-                case.offer(Bfs(bfs.q1, bfs.q2, bfs.sum1, bfs.sum2, bfs.count++, 2))
+                case.offer(Bfs(LinkedList<Int>(bfs.q1), LinkedList<Int>(bfs.q2), bfs.sum1 + poll, bfs.sum2 - poll, bfs.count+1, 1))
+                case.offer(Bfs(LinkedList<Int>(bfs.q1), LinkedList<Int>(bfs.q2), bfs.sum1 + poll, bfs.sum2 - poll, bfs.count+1, 2))
             }
 
         }
         return answer
     }
 
-    class Bfs(val q1: LinkedList<Int>, val q2: LinkedList<Int>, var sum1: Long, var sum2: Long, var count: Int, var turn: Int)
+    private fun checkPossible(total: IntArray, sum: Long): Boolean {
+        if(sum and 1L == 1L) return false
+        val queue = LinkedList<Check>()
+
+        queue.offer(Check(0, 0, sum / 2, total))
+
+        while (queue.isNotEmpty()) {
+            val check = queue.poll()
+            if(check.count == check.goal) return true
+            if(check.idx == total.lastIndex) continue
+            queue.offer(Check(check.idx + 1, check.count, check.goal, check.total))
+            queue.offer(Check(check.idx + 1, check.count + total[check.idx], check.goal, check.total))
+        }
+        return false
+    }
+
+    class Bfs(val q1: LinkedList<Int>, val q2: LinkedList<Int>, val sum1: Long, val sum2: Long, val count: Int, val turn: Int)
+    class Check(val idx: Int, val count: Long, val goal: Long, val total: IntArray)
+}
+
+fun main() {
+    val a = 두큐합_같게()
+    a.solution(intArrayOf(3,2,7,2), intArrayOf(4,6,5,1))
 }
