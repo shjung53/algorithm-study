0d30852a62295924b6c5fab67ee341ea5cbca551
shjung <gcjung777@gmail.com>
2023-10-26 22:47:09 +0900

    1026

--- src/ssafy/이가빠진이진트리_26260/Main.java
+++ src/ssafy/이가빠진이진트리_26260/Main.java
@@ -6,50 +6,34 @@ import java.io.*;
 public class Main {
 	static BufferedReader br;
 	static StringTokenizer st;
-	static Node[] nodes;
+	static int[] tree; //
+	static int[] values;
 	static int n;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine().trim());
 
-		nodes = new Node[n + 1];
-		int erasedIdx = 0;
+		tree = new int[n + 1]; // 1번 노드부터 n번 노드까지
+		values = new int[n]; // value는 n개
+		int erasedIdx = -1;
 
 		st = new StringTokenizer(br.readLine().trim());
 
 		for (int i = 1; i <= n; i++) {
 			int value = Integer.parseInt(st.nextToken());
-			nodes[i].parent = i / 2;
-			nodes[i].left = i * 2;
-			nodes[i].right = i * 2 + 1;
-			nodes[i].value = value;
 			if (value == -1)
 				erasedIdx = i;
 		}
 
-		int checkIdx = erasedIdx;
-		nodes[erasedIdx].value = Integer.parseInt(br.readLine().trim());
-
-		while (true) {
-			// 리프 노드 조건: idx가 2로 나눈 값보다 크면 트리의 마지막 레벨 - 자식이 없음
-			if (checkIdx > n / 2) {
-				// 홀수면 오른쪽 자식
-				if ((checkIdx & 1) == 1) {
-					if(nodes[checkIdx].value > nodes[checkIdx / 2].value) break;
-				} else {
-
-				}
-			}
+		values[erasedIdx] = Integer.parseInt(br.readLine().trim());
+		
+		Arrays.sort(values);
+		
+		for(int i=0; i<n; i++) {
+			tree[]
 		}
+
 	}
 }
 
-class Node {
-	int value, parent, left, right;
-
-	public Node(int value) {
-		super();
-		this.value = value;
-	}
-}
---
+++ src/ssafy/전단지돌리기_19542/Main.java
@@ -0,0 +1,14 @@
+package 전단지돌리기_19542;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static BufferedReader br;
+	static StringTokenizer st;
+
+	public static void main(String[] args) {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
