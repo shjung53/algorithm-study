cae01563d3ece15fc1344f8d553535c159b9628f
shjung <gcjung777@gmail.com>
2023-10-27 22:03:46 +0900

    1027

--- src/ssafy/이가빠진이진트리_26260/Main.java
+++ src/ssafy/이가빠진이진트리_26260/Main.java
@@ -6,34 +6,51 @@ import java.io.*;
 public class Main {
 	static BufferedReader br;
 	static StringTokenizer st;
-	static int[] tree; //
 	static int[] values;
 	static int n;
+	static ArrayDeque<Integer> stack;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine().trim());
+		StringBuilder stb = new StringBuilder();
+		stack = new ArrayDeque<>();
 
-		tree = new int[n + 1]; // 1번 노드부터 n번 노드까지
-		values = new int[n]; // value는 n개
+		values = new int[n + 1];
 		int erasedIdx = -1;
 
 		st = new StringTokenizer(br.readLine().trim());
 
 		for (int i = 1; i <= n; i++) {
 			int value = Integer.parseInt(st.nextToken());
+			values[i] = value;
 			if (value == -1)
 				erasedIdx = i;
 		}
 
 		values[erasedIdx] = Integer.parseInt(br.readLine().trim());
-		
+
 		Arrays.sort(values);
-		
-		for(int i=0; i<n; i++) {
-			tree[]
-		}
+
+		findMiddle(1, n);
+
+		while (!stack.isEmpty())
+			stb.append(stack.removeLast()).append(' ');
+
+		System.out.println(stb);
 
 	}
-}
 
+	public static void findMiddle(int start, int end) {
+
+		if (start == end) {
+			stack.addLast(values[start]);
+			return;
+		}
+
+		int middle = (start + end) / 2;
+		stack.addLast(values[middle]);
+		findMiddle(middle + 1, end);
+		findMiddle(start, middle - 1);
+	}
+}
--- src/ssafy/전단지돌리기_19542/Main.java
+++ src/ssafy/전단지돌리기_19542/Main.java
@@ -6,9 +6,77 @@ import java.io.*;
 public class Main {
 	static BufferedReader br;
 	static StringTokenizer st;
+	static int n;
+	static int s;
+	static int d;
+	static Node[] nodes;
+	static int answer;
+	static boolean[] visited;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
-		
+		st = new StringTokenizer(br.readLine().trim());
+		n = Integer.parseInt(st.nextToken());
+		s = Integer.parseInt(st.nextToken());
+		d = Integer.parseInt(st.nextToken());
+
+		nodes = new Node[n + 1];
+		for (int i = 1; i <= n; i++) {
+			nodes[i] = new Node();
+		}
+
+		for (int i = 0; i < n - 1; i++) {
+			st = new StringTokenizer(br.readLine().trim());
+			int node1 = Integer.parseInt(st.nextToken());
+			int node2 = Integer.parseInt(st.nextToken());
+			nodes[node1].link.add(node2);
+			nodes[node2].link.add(node1);
+		}
+
+		visited = new boolean[n + 1];
+		visited[s] = true;
+		nodes[s].distance = maxFromHere(s);
+		answer = 0;
+		visited = new boolean[n + 1];
+		visited[s] = true;
+		countDistance(s);
+		System.out.println(answer * 2);
 	}
+
+	private static void countDistance(int nodeIdx) {
+		if (nodes[nodeIdx].distance <= d)
+			return;
+
+		for (int nextNode : nodes[nodeIdx].link) {
+			if (visited[nextNode])
+				continue;
+			if (nodes[nextNode].distance < d)
+				continue;
+			visited[nextNode] = true;
+			answer++;
+			countDistance(nextNode);
+		}
+	}
+
+	private static int maxFromHere(int nodeIdx) {
+
+		boolean isLeaf = true;
+		for (int nextNode : nodes[nodeIdx].link) {
+			if (visited[nextNode])
+				continue;
+			isLeaf = false;
+			visited[nextNode] = true;
+			nodes[nodeIdx].distance = Math.max(nodes[nodeIdx].distance, maxFromHere(nextNode) + 1);
+		}
+
+		if (isLeaf)
+			return 0;
+
+		return nodes[nodeIdx].distance;
+	}
+}
+
+class Node {
+	int distance = 0; // 리프노드까지의 거리
+	ArrayList<Integer> link = new ArrayList<Integer>();
 }
