b0606825ee00b27646bd623b45c56ab0a317b747
shjung <gcjung777@gmail.com>
2023-11-05 23:20:07 +0900

    1105

---
+++ src/ssafy/구간합구하기5/Main.java
@@ -0,0 +1,64 @@
+package 구간합구하기5;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static BufferedReader br;
+	static StringTokenizer st;
+	static int[][] graph;
+	static int[][] dp;
+	static int n;
+	static int m;
+	static int startX;
+	static int startY;
+	static int endX;
+	static int endY;
+	static int sum;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder stb = new StringBuilder();
+		st = new StringTokenizer(br.readLine().trim());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		graph = new int[n + 1][n + 1];
+		dp = new int[n + 1][n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine().trim());
+			for (int j = 1; j <= n; j++) {
+				graph[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 1; i <= n; i++) {
+			findDp(i, n);
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine().trim());
+			startX = Integer.parseInt(st.nextToken());
+			startY = Integer.parseInt(st.nextToken());
+			endX = Integer.parseInt(st.nextToken());
+			endY = Integer.parseInt(st.nextToken());
+			sum = 0;
+
+			for (int x = startX; x <= endX; x++) {
+				sum += dp[x][endY] - dp[x][startY-1];
+			}
+
+			stb.append(sum).append('\n');
+		}
+
+		System.out.println(stb);
+	}
+
+	public static int findDp(int x, int y) {
+		if (y == 1)
+			return dp[x][y] = graph[x][y];
+		return dp[x][y] = findDp(x, y - 1) + graph[x][y];
+	}
+
+}
--- src/ssafy/최단경로_1753/Main.java
+++ src/ssafy/최단경로_1753/Main.java
@@ -8,9 +8,10 @@ public class Main {
 	static StringTokenizer st;
 	static int v; // 정점의 개수
 	static int e; // 간선의 개수
-	static int[][] connection; // 간선 연결 정보, 0보다 크면 간선존재, 값은 가중치 값
 	static int start; // 시작 정점
-	static int[] visited; // 정점 방문 시점의 가중치의 합
+	static Edge[] edges;
+	static Node[] nodes;
+	static int[] visited; // 정점 방문 시점의 가중치
 
 	public static void main(String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,55 +21,74 @@ public class Main {
 		start = Integer.parseInt(br.readLine().trim());
 		StringBuilder stb = new StringBuilder();
 
-		connection = new int[v + 1][v + 1];
+		nodes = new Node[v + 1];
+		edges = new Edge[e];
 
-		for (int inputIdx = 0; inputIdx < e; inputIdx++) {
+		for (int i = 0; i < v + 1; i++) {
+			nodes[i] = new Node();
+		}
+
+		for (int eIdx = 0; eIdx < e; eIdx++) {
 			st = new StringTokenizer(br.readLine().trim());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			int weight = Integer.parseInt(st.nextToken());
-
-			if (connection[from][to] == 0) {
-				connection[from][to] = weight;
-			} else {
-				if (weight < connection[from][to])
-					connection[from][to] = weight;
-			}
+			edges[eIdx] = new Edge(from, to, weight);
+			nodes[from].edges.add(eIdx);
 		}
 
-		for (int goal = 1; goal <= v; goal++) {
-			visited = new int[v + 1];
-			if (bfs(goal)) {
-				stb.append(visited[goal]);
-			} else {
-				stb.append("INF");
+		bfs(start);
+
+		for (int i = 1; i <= v; i++) {
+			if (i == start) {
+				stb.append(0).append('\n');
+				continue;
 			}
-			if (goal != v)
-				stb.append('\n');
+
+			if (nodes[i].weight == 0) {
+				stb.append("INF").append('\n');
+				continue;
+			}
+
+			stb.append(nodes[i].weight).append('\n');
 		}
 		System.out.println(stb.toString());
 	}
 
-	private static boolean bfs(int goal) {
-		boolean possible = false;
+	private static void bfs(int start) {
 		Queue<Integer> queue = new ArrayDeque<Integer>();
-		queue.offer(start);
+		queue.offer(start); // 시작노드를 넣는다.
 
 		while (!queue.isEmpty()) {
-			int now = queue.poll();
-			if (now == goal) {
-				possible = true;
-				break;
-			}
-			for (int vIdx = 1; vIdx <= v; vIdx++) {
-				if (connection[now][vIdx] > 0
-						&& (visited[vIdx] == 0 || visited[vIdx] > visited[now] + connection[now][vIdx])) {
-					visited[vIdx] = visited[now] + connection[now][vIdx];
-					queue.offer(vIdx);
+			int nowIdx = queue.poll();
+			Node now = nodes[nowIdx];
+
+			for (int eIdx = 0; eIdx < now.edges.size(); eIdx++) {
+				Edge edge = edges[now.edges.get(eIdx)];
+				if (nodes[edge.to].weight != 0) {
+					if (now.weight + edge.weight > nodes[edge.to].weight)
+						continue;
 				}
+				nodes[edge.to].weight = now.weight + edge.weight;
+				queue.offer(edge.to);
 			}
 		}
-
-		return possible;
 	}
+}
+
+class Edge {
+	int from;
+	int to;
+	int weight;
+
+	public Edge(int from, int to, int weight) {
+		this.from = from;
+		this.to = to;
+		this.weight = weight;
+	}
+}
+
+class Node {
+	int weight = 0;
+	ArrayList<Integer> edges = new ArrayList<Integer>();
 }
