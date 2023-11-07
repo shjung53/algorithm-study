764918ba92a88b1a9bbd28126888966bba799c1c
shjung <gcjung777@gmail.com>
2023-11-07 23:06:51 +0900

    1107

---
+++ src/ssafy/플로이드_11404/Main.java
@@ -0,0 +1,66 @@
+package 플로이드_11404;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static BufferedReader br;
+	static StringTokenizer st;
+	static int n;
+	static int m;
+	static int[][] link;
+	static int max = 100000000;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine().trim());
+		m = Integer.parseInt(br.readLine().trim());
+		link = new int[n + 1][n + 1];
+		StringBuilder stb = new StringBuilder();
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				link[i][j] = max;
+			}
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine().trim());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			if (link[from][to] > weight) {
+				link[from][to] = weight;
+			}
+		}
+
+		for (int k = 1; k <= n; k++) {
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= n; j++) {
+					if (link[i][j] > link[i][k] + link[k][j]) {
+						link[i][j] = link[i][k] + link[k][j];
+					}
+				}
+			}
+		}
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (link[i][j] >= max)
+					link[i][j] = 0;
+				if (i == j)
+					link[i][j] = 0;
+			}
+		}
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				stb.append(link[i][j]).append(' ');
+			}
+			stb.append('\n');
+		}
+
+		System.out.println(stb);
+
+	}
+}
