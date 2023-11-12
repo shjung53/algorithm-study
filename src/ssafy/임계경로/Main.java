cf8eb5c7cc91b0776e58b5d6c1658182da5a5b14
shjung <gcjung777@gmail.com>
2023-11-12 15:04:22 +0900

    1112

--- src/ssafy/임계경로/Main.java
+++ src/ssafy/임계경로/Main.java
@@ -24,7 +24,7 @@ public class Main {
 		int answerRoad = 0;
 
 		for (int i = 1; i <= n; i++) {
-			cities[i] = new City(m, i);
+			cities[i] = new City();
 		}
 
 		for (int i = 0; i < m; i++) {
@@ -33,56 +33,74 @@ public class Main {
 			int to = Integer.parseInt(st.nextToken());
 			int time = Integer.parseInt(st.nextToken());
 			roads[i] = new Road(i, from, to, time);
-			cities[from].roadTo.add(i);
-			cities[to].roadFrom.add(i);
+			cities[from].roadTo.offer(roads[i]);
+			cities[to].roadFrom.offer(roads[i]);
+			cities[to].roadFrom2.offer(roads[i]);
 		}
 
 		st = new StringTokenizer(br.readLine().trim());
 		departure = Integer.parseInt(st.nextToken());
 		destination = Integer.parseInt(st.nextToken());
 
-		Queue<Integer> queue = new ArrayDeque<>();
-		queue.offer(departure);
+		PriorityQueue<Temp> queue = new PriorityQueue<Temp>(new Comparator<Temp>() {
+			@Override
+			public int compare(Temp o1, Temp o2) {
+				// TODO Auto-generated method stub
+				return o2.time - o1.time;
+			}
+		});
+
+		queue.offer(new Temp(departure, 0));
 
 		while (!queue.isEmpty()) {
-			int nowCityNum = queue.poll();
-			City nowCity = cities[nowCityNum];
+			Temp nowCity = queue.poll();
 
-			if (nowCityNum == destination) {
-				answerTime = nowCity.time;
-
-				for (int i = 0; i < m; i++) {
-					if (nowCity.visited[i])
-						answerRoad++;
-				}
-				continue;
-			}
-
-			for (int roadNum : nowCity.roadTo) {
-				Road road = roads[roadNum];
+			while (!cities[nowCity.cityNum].roadTo.isEmpty()) {
+				Road road = cities[nowCity.cityNum].roadTo.poll();
 				int toCityNum = road.to;
-				cities[toCityNum].roadFrom.remove(roadNum); // 이동한 후 도시로 이동하는 도로 제거
+				cities[toCityNum].roadFrom.remove(road);
 
-				// 이동한 후의 시간이 가장 오래 걸린 시간이면 거친 도로 목록을 리셋 혹은 시간이 같으면 거친 도로 목록을 추가해준다.
+				// 이동한 후의 시간이 가장 오래 걸린 시간이면 시간 리셋.
 				if (nowCity.time + road.time > cities[toCityNum].time) {
 					cities[toCityNum].time = nowCity.time + road.time;
-					for (int i = 0; i < m; i++) {
-						if (nowCity.visited[i]) {
-							cities[toCityNum].visited[i] = true;
-						} else {
-							cities[toCityNum].visited[i] = false;
-						}
-					} // 오래 걸렸으면 이 도로 목록으로 업데이트
-					cities[toCityNum].visited[toCityNum] = true;
-				} else if (nowCity.time + road.time == cities[road.to].time) {
-					for (int i = 0; i < m; i++) {
-						if (nowCity.visited[i])
-							cities[toCityNum].visited[i] = true;
-					}
 				}
 
-				if (cities[road.to].roadFrom.size() == 0)
-					queue.offer(road.to); // 더 이상 들어오는 도로가 없으면 도시를 추가한다.
+				// 들어가는 도시가 더이상 없으면 큐에 넣기
+				if (cities[toCityNum].roadFrom.isEmpty())
+					queue.offer(new Temp(toCityNum, cities[toCityNum].time));
+			}
+		}
+
+		PriorityQueue<City> newQueue = new PriorityQueue<City>(1, new Comparator<City>() {
+
+			@Override
+			public int compare(City o1, City o2) {
+				// TODO Auto-generated method stub
+				return o2.time - o1.time;
+			}
+
+		});
+
+		answerTime = cities[destination].time;
+		newQueue.offer(cities[destination]);
+
+		boolean[] visited = new boolean[n + 1];
+		visited[destination] = true;
+
+		while (!newQueue.isEmpty()) {
+			City nowCity = newQueue.poll();
+
+			while (!nowCity.roadFrom2.isEmpty()) {
+				Road road = nowCity.roadFrom2.poll();
+
+				// 현재 도시의 시간에서 도로의 시간 뺀 값이 연결된 도시의 값과 같으면 최대시간이 걸린 도시
+				if (cities[road.from].time == nowCity.time - road.time) {
+					answerRoad++;
+					if (!visited[road.from]) {
+						visited[road.from] = true;
+						newQueue.offer(cities[road.from]);
+					}
+				}
 			}
 		}
 
@@ -108,15 +126,29 @@ class Road {
 	}
 }
 
-class City {
+class Temp {
+	int cityNum;
 	int time; // 이 도시까지온 가장 긴 시간
-	boolean[] visited; // 가장 긴 시간일때 거친 도로들
-	HashSet<Integer> roadTo = new HashSet<Integer>(); // 다른 도시로 가는 도로들
-	HashSet<Integer> roadFrom = new HashSet<Integer>(); // 이 도시를 향하는 도로들
 
-	public City(int m, int cityNum) {
+	public Temp(int cityNum, int time) {
 		super();
-		this.visited = new boolean[m];
-		this.visited[cityNum] = true;
+		this.cityNum = cityNum;
+		this.time = time;
 	}
+
+}
+
+class City {
+	int time = 0; // 이 도시까지 오는 시간
+	Comparator<Road> comparator = new Comparator<Road>() {
+
+		@Override
+		public int compare(Road o1, Road o2) {
+			// TODO Auto-generated method stub
+			return o2.time - o1.time;
+		}
+	};
+	PriorityQueue<Road> roadTo = new PriorityQueue<Road>(1, comparator); // 다른 도시로 가는 도로들
+	PriorityQueue<Road> roadFrom = new PriorityQueue<Road>(1, comparator); // 이 도시를 향하는 도로들
+	PriorityQueue<Road> roadFrom2 = new PriorityQueue<Road>(1, comparator); // 이 도시를 향하는 도로들2
 }
