8cfae8d31bd8ed9f4e5984299d0f2a39f88ba6d7
shjung <gcjung777@gmail.com>
2023-11-19 17:37:46 +0900

    1119

--- src/ssafy/거짓말_1043/Main.java
+++ src/ssafy/거짓말_1043/Main.java
@@ -10,8 +10,10 @@ public class Main {
 	static int n;
 	static int m;
 
-	static Set<Integer> truth;
+	static boolean[] truth;
 	static int[] parent;
+	static int[] rank;
+	static int[][] party = new int[51][51];
 	static int answer;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -21,32 +23,74 @@ public class Main {
 		m = Integer.parseInt(st.nextToken());
 
 		parent = new int[n + 1];
-		truth = new HashSet<Integer>();
+		truth = new boolean[n + 1];
+		rank = new int[n + 1];
 		answer = 0;
 
 		st = new StringTokenizer(br.readLine().trim());
 		int count = Integer.parseInt(st.nextToken());
-		
-		for(int i=1; i<=n; i++) {
+
+		for (int i = 1; i <= n; i++) {
 			parent[i] = i;
 		}
 
 		for (int i = 0; i < count; i++) {
 			int person = Integer.parseInt(st.nextToken());
-			truth.add(person);
+			truth[person] = true;
 		}
 
-		for (int i = 0; i < m; i++) {
+		// 각 파티별로 루트인 사람을 0번 인덱스에 넣어둔다
+		for (int i = 1; i <= m; i++) {
 			st = new StringTokenizer(br.readLine().trim());
 			int partyN = Integer.parseInt(st.nextToken()); // 파티에 온 사람 수
-			
+
+			int pre = 0;
 			for (int j = 0; j < partyN; j++) {
 				int partyPerson = Integer.parseInt(st.nextToken());
+				if (pre == 0) {
+					pre = partyPerson;
+					continue;
+				}
+				union(pre, partyPerson);
+				pre = partyPerson;
 			}
+			party[i][0] = pre;
 		}
-		
-		
-		System.out.println(answer);
 
+		for (int i = 1; i <= m; i++) {
+			// 파티의 루트를 재설정
+			party[i][0] = findParent(party[i][0]);
+			if (!truth[party[i][0]])
+				answer++;
+		}
+		System.out.println(answer);
+	}
+
+	private static void union(int x, int y) {
+		int xRoot = findParent(x);
+		int yRoot = findParent(y);
+		if (xRoot == yRoot)
+			return;
+
+		if (rank[yRoot] > rank[xRoot]) {
+			int temp = xRoot;
+			xRoot = yRoot;
+			yRoot = temp;
+		}
+
+		// 둘중 하나라도 진실을 알면 진실로 만들어야 한다.
+		if (truth[xRoot] || truth[yRoot]) {
+			truth[xRoot] = true;
+			truth[yRoot] = true;
+		}
+
+		parent[yRoot] = xRoot;
+		rank[xRoot]++;
+	}
+
+	private static int findParent(int person) {
+		if (parent[person] == person)
+			return person;
+		return parent[person] = findParent(parent[person]);
 	}
 }
