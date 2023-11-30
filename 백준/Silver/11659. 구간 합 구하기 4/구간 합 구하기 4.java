b462c7c791f75aac7abbd4eb77f98c6f09d35192
shjung53 <90888718+shjung53@users.noreply.github.com>
2023-11-30 21:04:27 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 568 ms, Memory: 57964 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static BufferedReader br;
+	static StringTokenizer st;
+	static int[] sum; // 1부터 index까지의 합
+	static int n;
+	static int m;
+	static int s;
+	static int e;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder stb = new StringBuilder();
+		st = new StringTokenizer(br.readLine().trim());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		sum = new int[n + 1];
+
+		st = new StringTokenizer(br.readLine().trim());
+		for (int i = 1; i <= n; i++) {
+			sum[i] = Integer.parseInt(st.nextToken()) + sum[i - 1];
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine().trim());
+			s = Integer.parseInt(st.nextToken());
+			e = Integer.parseInt(st.nextToken());
+
+			stb.append(sum[e] - sum[s - 1]).append('\n');
+		}
+
+		System.out.println(stb);
+	}
+
+}
