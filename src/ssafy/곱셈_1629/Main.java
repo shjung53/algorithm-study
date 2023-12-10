7dfcb52c6a91f565995aad375ecf62c02fc03301
shjung <gcjung777@gmail.com>
2023-12-11 00:39:17 +0900

    1211

---
+++ src/ssafy/곱셈_1629/Main.java
@@ -0,0 +1,33 @@
+package 곱셈_1629;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static BufferedReader br;
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine().trim());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+
+		System.out.println(myPow(a, b, c));
+	}
+
+	static long myPow(int a, int b, int c) {
+		if (b == 0)
+			return 1;
+
+		long n = myPow(a, b / 2, c);
+
+		if (b % 2 == 0) {
+			return n * n % c;
+		} else {
+			return (n * n % c) * a % c;
+		}
+	}
+}
