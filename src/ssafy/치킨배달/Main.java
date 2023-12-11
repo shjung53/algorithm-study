6fbaff590d1a900cc2583996a8e89e1609bb412d
shjung <gcjung777@gmail.com>
2023-12-11 21:18:42 +0900

    1211

--- src/ssafy/치킨배달/Main.java
+++ src/ssafy/치킨배달/Main.java
@@ -7,23 +7,84 @@ public class Main {
 
 	static int[] dx = { 1, 1, 1, 0, -1, -1, -1, 0 };
 	static int[] dy = { -1, 0, 1, 1, 1, 0, -1, -1 };
-	static boolean[][] visited;
-	static int[][] board;
+	static int n;
+	static int m;
+	static ArrayList<Chicken> chickens;
+	static ArrayList<House> houses;
+	static boolean[] maintain;
+	static int answer;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		board = new int[n][n];
-		visited = new boolean[n][n];
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		chickens = new ArrayList<Chicken>();
+		houses = new ArrayList<House>();
+		answer = Integer.MAX_VALUE;
 
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < n; j++) {
-				board[i][j] = Integer.parseInt(st.nextToken());
+				int num = Integer.parseInt(st.nextToken());
+				if (num == 1)
+					houses.add(new House(i, j));
+				if (num == 2)
+					chickens.add(new Chicken(i, j));
 			}
 		}
 
+		maintain = new boolean[chickens.size()];
+
+		chooseChicken(0, 0);
+		System.out.println(answer);
+
+	}
+
+	private static void chooseChicken(int idx, int count) {
+
+		if (count == m) {
+			int distance = 0;
+			for (House house : houses) {
+				// 집에서 가장 가까운 치킨집찾기
+				int minDistance = Integer.MAX_VALUE;
+				for (int i = 0; i < maintain.length; i++) {
+					if (!maintain[i])
+						continue; // 닫았으면 패스
+					int cal = Math.abs(chickens.get(i).y - house.y) + Math.abs(chickens.get(i).x - house.x);
+					minDistance = Math.min(minDistance, cal);
+				}
+				distance += minDistance; // 최소거리 더하기
+			}
+
+			answer = Math.min(answer, distance);
+			return;
+		}
+
+		if (idx >= chickens.size())
+			return;
+
+		maintain[idx] = true;
+		chooseChicken(idx + 1, count + 1);
+		maintain[idx] = false;
+		chooseChicken(idx + 1, count);
+	}
+}
+
+class Chicken {
+	int y, x;
+
+	public Chicken(int y, int x) {
+		this.y = y;
+		this.x = x;
+	}
+}
+
+class House {
+	int y, x;
+
+	public House(int y, int x) {
+		this.y = y;
+		this.x = x;
 	}
 }
