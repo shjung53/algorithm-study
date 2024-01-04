24a770919243f6534cd487d0be60a4ead1efa4ec
shjung <gcjung777@gmail.com>
2024-01-04 21:37:32 +0900

    0104

---
+++ src/ssafy/추월_2002/Main.java
@@ -0,0 +1,43 @@
+package 추월_2002;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	static BufferedReader br;
+	static StringTokenizer st;
+	static int n;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine().trim());
+		int answer = 0;
+		HashSet<String> set = new HashSet<>();
+		Queue<String> queue = new ArrayDeque<>();
+		
+		for(int i=0; i<n; i++) {
+			String carNumber = br.readLine();
+			set.add(carNumber);
+			queue.offer(carNumber);
+		}
+		
+		for(int i=0; i<n; i++) {
+			String carNumber = br.readLine();
+			while(!set.contains(queue.peek())) {
+				queue.poll();
+			}
+			if(queue.peek().equals(carNumber)) {
+				queue.poll();
+				set.remove(carNumber);
+			}else {
+				if(set.contains(carNumber)) {
+					answer++;
+					set.remove(carNumber);
+				}
+			}
+		}
+		
+		System.out.println(answer);
+	}
+}
