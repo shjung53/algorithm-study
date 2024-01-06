4ba68c1b9d53cf129444b4fea86754066ae62642
shjung <gcjung777@gmail.com>
2024-01-06 17:34:05 +0900

    240106

--- src/ssafy/포도주시식_2156/Main.java
+++ src/ssafy/포도주시식_2156/Main.java
@@ -1,6 +1,5 @@
 package ssafy.포도주시식_2156;
 
-import java.util.*;
 import java.io.*;
 
 public class Main {
@@ -15,12 +14,23 @@ public class Main {
         arr = new int[n];
         dp = new int[n];
 
-        for(int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine().trim());
         }
 
-        for(int i=0; i<n; i++) {
-            dp[i] = Math.max(arr[i-1] + dp[i-2], dp[i-2] + arr[i]);
+        for (int i = 0; i < n; i++) {
+            dp[i] = getDp(i);
         }
+
+        System.out.println(dp[n - 1]);
+    }
+
+    public static int getDp(int idx) {
+        if (idx == 0) return dp[idx] = arr[0];
+        if (idx == 1) return dp[idx] = arr[0] + arr[1];
+        if (idx == 2) return Math.max(Math.max(arr[0] + arr[1], arr[0] + arr[2]), arr[1] + arr[2]);
+        if (dp[idx] == 0)
+            return dp[idx] = Math.max(Math.max(dp[idx - 1], dp[idx - 2] + arr[idx]), dp[idx - 3] + arr[idx - 1] + arr[idx]);
+        return dp[idx];
     }
 }
