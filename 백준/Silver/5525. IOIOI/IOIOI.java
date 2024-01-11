cc83f7bd7a6de414fea488bbb9866c150313da72
shjung53 <90888718+shjung53@users.noreply.github.com>
2024-01-12 00:05:22 +0900

    [Silver I] Title: IOIOI, Time: 264 ms, Memory: 20128 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -4,22 +4,29 @@ import java.io.*;
 public class Main {
     static BufferedReader br;
     static int n, s;
+
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine().trim());
         s = Integer.parseInt(br.readLine().trim());
         int answer = 0;
 
-        StringBuilder stb = new StringBuilder("I");
-        for(int i=0; i<n; i++) {
-            stb.append("OI");
-        }
-        StringBuilder stb2 = new StringBuilder(br.readLine().trim());
-        String pn = stb.toString();
+        String str = br.readLine().trim();
+        char beforeChar = 'O';
+        int count = 0;
 
-        for(int idx=0; idx <= s - pn.length(); idx++) {
-            if(stb2.substring(idx, idx + pn.length()).equals(pn)) answer++;
+        for (int idx = 0; idx < s; idx++) {
+            if (beforeChar == str.charAt(idx)) {
+                if ((count - 1) / 2 >= n) answer += 1 + ((count - 1) / 2) - n;
+                count = 0;
+                if(beforeChar == 'I') count = 1;
+            } else {
+                count++;
+            }
+            beforeChar = str.charAt(idx);
         }
+
+        if ((count - 1) / 2 >= n) answer += 1 + ((count - 1) / 2) - n;
 
         System.out.println(answer);
     }
