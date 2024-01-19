e2ca8e6b74036adfa359969c2f65e25bac42a247
shjung53 <90888718+shjung53@users.noreply.github.com>
2024-01-20 01:01:21 +0900

    [Silver IV] Title: 보물, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+
+    static int[] A, B;
+    static int n, answer;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine().trim());
+        A = new int[n];
+        B = new int[n];
+        st = new StringTokenizer(br.readLine().trim());
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine().trim());
+        for (int i = 0; i < n; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        PriorityQueue<Integer> aQueue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+
+        PriorityQueue<Integer> bQueue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        });
+
+        for(int i=0; i<n; i++) {
+            aQueue.add(A[i]);
+            bQueue.add(B[i]);
+        }
+
+        while(!aQueue.isEmpty()) {
+            answer += aQueue.poll() * bQueue.poll();
+        }
+
+        System.out.println(answer);
+
+
+    }
+}
