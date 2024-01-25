da937848869324932f3bca08e4ddbd46ffc93920
shjung53 <90888718+shjung53@users.noreply.github.com>
2024-01-25 21:52:50 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    static int[] dp;
+
+    static BufferedReader br;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine().trim());
+        dp = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            dp[i] = 5001;
+        }
+
+        if(n >= 3) dp[3] = 1;
+        if(n >= 5) dp[5] = 1;
+
+        for (int i = 6; i <= n; i++) {
+            dp[i] = Math.min(dp[i - 3] + 1, dp[i - 5] + 1);
+        }
+
+        if(dp[n] > 5000) dp[n] = -1;
+
+        System.out.println(dp[n]);
+    }
+
+}
