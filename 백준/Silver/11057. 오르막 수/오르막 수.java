a8d4c9c07b8861007dde0158bb596e63245b9b06
shjung53 <90888718+shjung53@users.noreply.github.com>
2024-01-26 22:02:54 +0900

    [Silver I] Title: 오르막 수, Time: 120 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    static int[][] dp;
+
+    static BufferedReader br;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine().trim());
+
+        dp = new int[n + 1][10];
+
+        for (int i = 0; i <= 9; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j <= 9; j++) {
+                if (j > 0) {
+                    dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 10007;
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < 10; i++) {
+            answer += dp[n][i];
+        }
+
+        System.out.println(answer % 10007);
+    }
+}
