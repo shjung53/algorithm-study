95ea1885421b97a645a9c03ccab3084848080110
shjung53 <90888718+shjung53@users.noreply.github.com>
2024-01-28 15:17:30 +0900

    [Gold IV] Title: 고층 건물, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Gold/1027. 고층 건물/고층 건물.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int[] height;
+    static int[] countArr;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine().trim());
+        st = new StringTokenizer(br.readLine().trim());
+        countArr = new int[n];
+        height = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            height[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int standard = 0; standard < n; standard++) {
+
+            int left = standard - 1;
+            int right = standard + 1;
+
+            double leftMaxDecline = Integer.MIN_VALUE;
+            double rightMaxDecline = Integer.MIN_VALUE;
+            int count = 0;
+
+            while (left >= 0) {
+                if (leftMaxDecline < (double) (height[left] - height[standard]) / (standard - left)) {
+                    leftMaxDecline = ((double) (height[left] - height[standard]) / (standard - left));
+                    count++;
+                }
+                left--;
+            }
+
+            while (right < n) {
+                if (rightMaxDecline < (double) (height[right] - height[standard]) / (right - standard)) {
+                    rightMaxDecline = (double) (height[right] - height[standard]) / (right - standard);
+                    count++;
+                }
+                right++;
+            }
+
+            countArr[standard] = count;
+        }
+
+        int answer = 0;
+        for (int count : countArr) {
+            if (answer < count) answer = count;
+        }
+
+        System.out.println(answer);
+    }
+}
