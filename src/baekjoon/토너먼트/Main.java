fd63001c6723f0489835f5fc1b7294a7dd117c01
정수현 <gcjung777@naver.com>
2024-02-11 22:23:39 +0900

    0211

---
+++ src/baekjoon/토너먼트/Main.java
@@ -0,0 +1,31 @@
+package baekjoon.토너먼트;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br;
+
+    static StringTokenizer st;
+
+    static int n, a, b;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine().trim());
+        n = Integer.parseInt(st.nextToken());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        int round = 0;
+
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            round++;
+        }
+
+        System.out.println(round);
+    }
+}
