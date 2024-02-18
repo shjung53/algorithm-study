8d61fbf0f8b4908639ff57cf1e0cd704cc6e1a1e
정수현 <gcjung777@naver.com>
2024-02-18 21:47:23 +0900

    0218

---
+++ src/baekjoon/단어정렬_1181/Main.java
@@ -0,0 +1,40 @@
+package baekjoon.단어정렬_1181;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br;
+
+    static int n;
+
+    static String[] arr;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine().trim());
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine().trim());
+        }
+
+        arr = new String[set.size()];
+
+        arr = set.toArray(arr);
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() != o2.length()) {
+                    return o1.length() - o2.length();
+                } else {
+                    return o1.compareTo(o2);
+                }
+            }
+        });
+
+        for (String s : arr) {
+            System.out.println(s);
+        }
+    }
+}
