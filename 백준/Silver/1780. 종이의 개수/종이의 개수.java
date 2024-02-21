1784816eb8d235da0a65a1a1cb999d424782d3d2
shjung53 <90888718+shjung53@users.noreply.github.com>
2024-02-21 23:05:08 +0900

    [Silver II] Title: 종이의 개수, Time: 908 ms, Memory: 309260 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+
+    static int n, zero, one, minusOne;
+
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine().trim());
+        map = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine().trim());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        zero = 0;
+        one = 0;
+        minusOne = 0;
+        countPaper(0, 0, n);
+
+        System.out.println(minusOne);
+        System.out.println(zero);
+        System.out.println(one);
+
+    }
+
+    private static void countPaper(int y, int x, int length) {
+        int number = map[y][x];
+        boolean same = true;
+
+        Loop:
+        for (int i = y; i < y + length; i++) {
+            for (int j = x; j < x + length; j++) {
+                if (map[i][j] != number) {
+                    same = false;
+                    break Loop;
+                }
+            }
+        }
+
+        int nextLength = length / 3;
+
+        if (!same) {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    countPaper(y + nextLength * i, x + nextLength * j, nextLength);
+                }
+            }
+        } else {
+             if(number == 1) one++;
+             if(number == -1) minusOne++;
+             if(number == 0) zero++;
+        }
+    }
+}
