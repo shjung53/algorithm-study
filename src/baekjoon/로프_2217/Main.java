7b827bfb2dd13bf3ef2f59987147f67444f8f9c3
정수현 <gcjung777@naver.com>
2024-02-25 16:30:40 +0900

    0225

---
+++ src/baekjoon/로프_2217/Main.java
@@ -0,0 +1,33 @@
+package baekjoon.로프_2217;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    static BufferedReader br;
+
+    static int n, min, answer;
+
+    static int[] weights;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine().trim());
+        weights = new int[n];
+        min = Integer.MAX_VALUE;
+        answer = Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++) {
+            weights[i] = Integer.parseInt(br.readLine().trim());
+            if(min > weights[i]) min = weights[i];
+        }
+
+        Arrays.sort(weights);
+
+        for(int i=0; i<n; i++) {
+            if(answer < weights[i] * (n - i)) answer = weights[i] * (n - i);
+        }
+
+        System.out.println(answer);
+    }
+}
